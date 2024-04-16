6b2eaacfc254b0ef476e77192991d6bdb12bb9bd
wh1tePigeon <90470778+wh1tePigeon@users.noreply.github.com>
2024-04-16 12:42:06 +0300

    del invalid links

--- download_model.py
+++ download_model.py
@@ -4,13 +4,14 @@ import gdown
 
 
 URL_LINKS = {
-    "main": "https://drive.google.com/file/d/1fXtT6g4hg9y-8Az4IQEmkwHZep1gI7MJ/view?usp=sharing",
-    "main_no_abs": "https://drive.google.com/file/d/1eTqIl7nsmBNWsb50gcGJrdZcm_tatde_/view?usp=sharing",
-    "main_no_weight_ce": "https://drive.google.com/file/d/1dSil-oF7BG4pPRiVVfoNlLjLB3i9yrKP/view?usp=sharing",
-    "main_non_zero_hz": "https://drive.google.com/file/d/1bCjIa8GXMzXdwrW_kQgvGqUpei2F2U1b/view?usp=sharing",
-    "main_dif_conv": "https://drive.google.com/file/d/1fXJVCQbAB-8BUdTXlt6fdmb7M9AWLGrR/view?usp=sharing",
-    "main_no_bn": "https://drive.google.com/file/d/1mf8nG9z-Zzg4p90lA-4iaVXVIxmZZRS7/view?usp=sharing",
-    "main_s3" : "https://drive.google.com/file/d/1UIwTBUMzZjNmWxZfi-KvUnP6HgDyyEYw/view?usp=sharing"
+    "main":
+    "main_no_abs":
+    "main_no_weight_ce":
+    "main_non_zero_hz":
+    "main_dif_conv":
+    "main_no_bn":
+    "main_s3":
+    #no valid links yet
 }
 
 SAVE_PATH = Path("test_model/")
